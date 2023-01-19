98b871ea0e6300359e0b9c74167222e6531e9813
mstreitner <marco.streitner@cloudflight.io>
2023-01-19 13:48:07 +0100

    #10 add MSSQL Server support

---
+++ testprojects/jdbc/mssql/src/main/kotlin/io/cloudflight/testresources/springboot/jdbc/mssql/Application.kt
@@ -0,0 +1,20 @@
+package io.cloudflight.testresources.springboot.jdbc.mssql
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.JpaRepository
+
+@SpringBootApplication
+class Application
+
+@Entity
+class Person(var name: String) {
+
+    @Id
+    @GeneratedValue
+    var id: Long = 0
+}
+
+interface PersonRepository : JpaRepository<Person, Long>
