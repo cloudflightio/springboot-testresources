809a712b407c725619f5598d6868ade6cbf4d530
Klaus Lehner <172195+klu2@users.noreply.github.com>
2023-03-03 08:21:20 +0100

    update to Spring Boot 3.0.3, Kotlin 1.8, remove JUnit4 from the classpath

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("io.cloudflight.autoconfigure-settings") version "0.8.11"
+    id("io.cloudflight.autoconfigure-settings") version "0.9.1"
 }
 
 rootProject.name = "springboot-testresources"
